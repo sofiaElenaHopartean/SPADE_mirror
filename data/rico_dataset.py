7022be7c2c575f353e4044a473f2b440debd9532
sofia.hopartean <sofia.hopartean@teleporthq.io>
2019-05-03 17:33:56 +0300

    Changes RICO  dataset script

--- data/rico_dataset.py
+++ data/rico_dataset.py
@@ -23,7 +23,7 @@ class RicoDataset(Pix2pixDataset):
         parser.set_defaults(crop_size=256)
         parser.set_defaults(display_winsize=256)
         parser.set_defaults(label_nc=26)
-        parser.set_defaults(contain_dontcare_label=False)
+        parser.set_defaults(contain_dontcare_label=True)
         parser.set_defaults(cache_filelist_read=False)
         parser.set_defaults(cache_filelist_write=False)
 
